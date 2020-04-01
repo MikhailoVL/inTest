99184fe2bf61ab664d36de9653b53c53cdb0da6e
MikhailoVL <mlyutyyv@gmail.com>
2020-04-01 23:18:59 +0300

    change

--- replace.py
+++ replace.py
@@ -14,22 +14,18 @@ def replace(path, old_string, new_string, line_number=None):
         print(f"File path {path} does not exist. Exiting...")
         sys.exit()
 
-    if line_number:
+    if not line_number:
         # line number not passed
         with open(path, "w") as f:
             f.write(new_string)
     else:
         with open(path, "r") as f:
             data = f.readlines()
-        count = len(data)
-        i = 1
-        while count > 0:
-            if i == line_number:
-                data[i - 1] = data[i - 1].replace(old_string, new_string)
-            i += 1
-            count -= 1
+
+        data[line_number-1] = data[line_number-1].replace(old_string, new_string)
+
         with open(path, "w") as f:
             f.writelines(data)
 
 
-replace_or_remove("wer.txt", "sderr", "wer", 2)
+replace("wer.txt", "sderr", "wer", 2)
